8a9f6400091ddc51da8610a7abdee89c0ae44987
GideonAg <gideonagbosu72@gmail.com>
2022-09-02 21:38:30 +0000

    Reduced width of UserInterface

--- app/UserInterface.java
+++ app/UserInterface.java
@@ -16,7 +16,7 @@ public class UserInterface extends JFrame {
     UserInterface(){
         this.setTitle("Finding possible paths to your destination");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(13000, 550);
+        this.setSize(1300, 550);
         this.setLayout(null);
         this.setResizable(false);
         this.getContentPane().setBackground(Color.cyan);
