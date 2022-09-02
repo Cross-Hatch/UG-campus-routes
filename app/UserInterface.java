fa7d3f59a0edfe743f02c8938bbf9a50bab1cb6a
GideonAg <gideonagbosu72@gmail.com>
2022-09-02 23:58:13 +0000

    Added a location name to places array

--- app/UserInterface.java
+++ app/UserInterface.java
@@ -42,7 +42,7 @@ public class UserInterface extends JFrame {
         String[] places =
                 {"Main Gate","Law school", "Night Market", "UG Fire Service", "CS Department", "Athletic Oval"
                         ,"Diaspora","Volta Hall","Akuafo Hall","Business School", "Legon Hall", "Great Hall",
-                        "NNB", "N Block", "JQB", "Balme Library","UGCS", "Pentagon Hall"};
+                        "NNB", "N Block", "Common wealth hall", "JQB", "Balme Library","UGCS", "Pentagon Hall"};
 
         sourceCombo = new JComboBox<>(places);
         sourceCombo.setBounds(550, 50, 200, 30);
